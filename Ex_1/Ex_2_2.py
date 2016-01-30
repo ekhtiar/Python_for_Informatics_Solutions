8359afb347667fa42913adaf849a8fe2b78c9cdf
Ekhtiar Syed <ekhtiar.syed@gmail.com>
2016-01-30 15:22:05 +0100

    Excercises from Chapter 2

--- Ex_1/Ex_2_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#adjust your shebang line
-
-#Excercise 2.2: Write a program that uses raw_input to prompt a user for their name and then welcomes them.
-#take input and store in name variable
-name = raw_input("What is your name? \n");
-#print name
-print("Hello " +name);
